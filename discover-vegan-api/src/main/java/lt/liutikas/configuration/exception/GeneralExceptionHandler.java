2bfc4d55fd84ba9b401794b251917e759e86f663
Matas Liutikas <matasliutikas@gmail.com>
2021-04-19 20:05:42 +0300

    added error handling for bad request

--- discover-vegan-api/src/main/java/lt/liutikas/configuration/exception/GeneralExceptionHandler.java
+++ discover-vegan-api/src/main/java/lt/liutikas/configuration/exception/GeneralExceptionHandler.java
@@ -2,8 +2,13 @@ package lt.liutikas.configuration.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
@@ -13,6 +18,24 @@ public class GeneralExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setErrorMessage(ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorMessage(processFieldErrorsMessage(ex));
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    private String processFieldErrorsMessage(MethodArgumentNotValidException ex) {
+        StringBuilder errorMessage = new StringBuilder();
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        for (org.springframework.validation.FieldError fieldError : fieldErrors) {
+            errorMessage.append("'").append(fieldError.getField()).append("' ").append(fieldError.getDefaultMessage()).append(", ");
+        }
+
+        return errorMessage.toString();
     }
 
 }
