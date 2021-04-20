85e90a4ac6701ee90f996c7178fc13b1c415e1d5
Matas Liutikas <matasliutikas@gmail.com>
2021-04-20 20:51:40 +0300

    added happy test to product creation

---
+++ discover-vegan-api/src/test/java/lt/liutikas/service/ProductServiceTest.java
@@ -0,0 +1,58 @@
+package lt.liutikas.service;
+
+import lt.liutikas.assembler.ProductAssembler;
+import lt.liutikas.dto.CreateProductDto;
+import lt.liutikas.entity.Product;
+import lt.liutikas.repository.ProductRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private ProductService productService;
+
+    @Before
+    public void setUp() {
+        productService = new ProductService(new ProductAssembler(), productRepository);
+    }
+
+    @Test
+    public void createProduct_givenAllData_createdProduct() {
+
+        CreateProductDto createProductDto = new CreateProductDto() {{
+            setName("Tofu");
+            setProducer("Sun wheat");
+        }};
+
+        Product product = new Product() {{
+            setProductId(1);
+            setName("Tofu");
+            setProducer("Sun wheat");
+            setImageUrl("https://www.test.com/image.png");
+        }};
+
+        when(productRepository.save(any(Product.class)))
+                .thenReturn(product);
+
+        Product createdProduct = productService.createProduct(createProductDto);
+
+        verify(productRepository, times(1))
+                .save(any(Product.class));
+
+        assertEquals(product.getProductId(), createdProduct.getProductId());
+        assertEquals(product.getName(), createdProduct.getName());
+        assertEquals(product.getProducer(), createdProduct.getProducer());
+        assertEquals(product.getImageUrl(), createdProduct.getImageUrl());
+    }
+}
