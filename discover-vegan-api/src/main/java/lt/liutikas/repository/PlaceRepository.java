c56a2977a77991d5ca20cdb8f2b8cee4f348945d
Matas Liutikas <matasliutikas@gmail.com>
2021-04-22 14:25:16 +0300

    added pagination

--- discover-vegan-api/src/main/java/lt/liutikas/repository/PlaceRepository.java
+++ discover-vegan-api/src/main/java/lt/liutikas/repository/PlaceRepository.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Component
 public class PlaceRepository {
 
-    private static final String FOOD_PLACES_URL = "/maps/api/place/nearbysearch/json?location={location}&radius={radius}&type=supermarket";
+    private static final String FOOD_PLACES_ENDPOINT = "/maps/api/place/nearbysearch/json?location={location}&radius={radius}&keyword=food&pagetoken={next_page_token}";
     private static final int SEARCH_RADIUS = 5000;
 
     private final RestTemplate googleRestTemplate;
@@ -26,18 +27,27 @@ public class PlaceRepository {
 
         String coordinates = String.format("%f,%f", getVendorDto.getLatitude(), getVendorDto.getLongitude());
 
-        ResponseEntity<PlacesResponse> responseEntity = googleRestTemplate.getForEntity(
-                FOOD_PLACES_URL,
-                PlacesResponse.class,
-                coordinates,
-                String.valueOf(SEARCH_RADIUS));
 
-        PlacesResponse placesResponse = responseEntity.getBody();
+        List<PlaceDto> places = new ArrayList<>();
+        PlacesResponse placesResponse = new PlacesResponse();
 
-        if (placesResponse.getResults() == null) {
-            return Collections.emptyList();
-        }
+        do {
+            ResponseEntity<PlacesResponse> responseEntity = googleRestTemplate.getForEntity(
+                    FOOD_PLACES_ENDPOINT,
+                    PlacesResponse.class,
+                    coordinates,
+                    String.valueOf(SEARCH_RADIUS),
+                    placesResponse.getNext_page_token());
 
-        return placesResponse.getResults();
+            placesResponse = responseEntity.getBody();
+
+            if (placesResponse.getResults() == null) {
+                return Collections.emptyList();
+            }
+
+            places.addAll(placesResponse.getResults());
+        } while (placesResponse.getNext_page_token() != null);
+
+        return places;
     }
 }
