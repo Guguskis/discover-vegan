808c7d5507a3045eddf7d5fff873c569103614fe
Matas Liutikas <matasliutikas@gmail.com>
2021-05-05 15:33:01 +0300

    renamed module

--- search-service/src/main/java/lt/liutikas/SearchServiceApplication.java
+++ search-service/src/main/java/lt/liutikas/SearchServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscoverVeganApiApplication {
+public class SearchServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoverVeganApiApplication.class, args);
+        SpringApplication.run(SearchServiceApplication.class, args);
     }
 
 }
